b5f50b2b494b29c85e0ff931aae3b83d4439b3dc
Ian Simons <iansimons93@gmail.com>
2024-12-17 14:08:10 -0500

    learning cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb: src/main.rs
+
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/hello_cargo/target/debug/deps/hello_cargo-ac6b107c143825fb.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/hello_cargo/target/debug/hello_cargo: /mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/hello_cargo/src/main.rs
