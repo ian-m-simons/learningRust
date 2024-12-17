5e3605481edca9ebe8366b6af77aa0e13e228722
Ian Simons <iansimons93@gmail.com>
2024-12-17 14:14:13 -0500

    learning cargo build options

---
+++ hello_cargo/target/release/deps/hello_cargo-5d87010d1c3d6827.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/hello_cargo/target/release/deps/hello_cargo-5d87010d1c3d6827: src/main.rs
+
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/hello_cargo/target/release/deps/hello_cargo-5d87010d1c3d6827.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/hello_cargo/target/release/hello_cargo: /mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/hello_cargo/src/main.rs
