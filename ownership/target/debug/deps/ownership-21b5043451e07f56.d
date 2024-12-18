e33c69913f863694f22087930a3712874207f322
Ian Simons <iansimons93@gmail.com>
2024-12-18 13:40:14 -0500

    quick string explanation

---
+++ ownership/target/debug/deps/ownership-21b5043451e07f56.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/ownership/target/debug/deps/ownership-21b5043451e07f56: src/main.rs
+
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/ownership/target/debug/deps/ownership-21b5043451e07f56.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/ownership/target/debug/ownership: /mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/ownership/src/main.rs
