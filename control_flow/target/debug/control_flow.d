9913eaca3e821d3608d4801dc46a631051f0423a
Ian Simons <iansimons93@gmail.com>
2024-12-18 11:03:02 -0500

    if statements

---
+++ control_flow/target/debug/control_flow.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/control_flow/target/debug/control_flow: /mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/control_flow/src/main.rs
---
+++ control_flow/target/debug/deps/control_flow-da7a2c8b36504c2b.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/control_flow/target/debug/deps/control_flow-da7a2c8b36504c2b: src/main.rs
+
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/control_flow/target/debug/deps/control_flow-da7a2c8b36504c2b.d: src/main.rs
+
+src/main.rs:
