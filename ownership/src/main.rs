b055853b863ab8dcffe521df68ef78cb82f6585c
Ian Simons <iansimons93@gmail.com>
2024-12-18 16:15:11 -0500

    mutable references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let s1 = String::from("hello");
+    let mut s1 = String::from("hello");
 
-    let len = calculate_length(&s1);
-    println!("The length of '{s1}' is {len}.");
+    change(&mut s1);
+    println!("{s1}");
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
