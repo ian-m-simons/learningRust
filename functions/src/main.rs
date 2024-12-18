087315c22010330cab35fe2bb1e7bcd5608c9c01
Ian Simons <iansimons93@gmail.com>
2024-12-18 10:36:31 -0500

    taking multiple parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    another_function(5);
+    another_function(5, 'h');
 }
 
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+fn another_function(x: i32, y: char) {
+    println!("The value of x is: {x}\nThe Units are {y}");
 }
