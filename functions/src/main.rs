2d492dbfc66acc1d84b23925a3f8a3507c4791f5
Ian Simons <iansimons93@gmail.com>
2024-12-18 10:55:03 -0500

    adding comments

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,14 @@
-fn main() {
-    let q = another_function(5,3);
+/*this is a block comment
+ * I just needed to clarify that this works
+ * thank you for your time
+ */
 
-    println!("The Value of q is {q}"); 
+fn main() {
+    let q = another_function(5,3);//setting value of q
+    
+    println!("The Value of q is {q}"); //printing q for the user
 }
 
 fn another_function(x: i32, y: i32) -> i32 {
-    x+y
+    x+y //calculating value to return
 }
