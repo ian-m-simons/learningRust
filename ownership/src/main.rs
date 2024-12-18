d171df29000ee43eb4b86c2614eeec6366ec7552
Ian Simons <iansimons93@gmail.com>
2024-12-18 14:15:20 -0500

    I wish references were a thing

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,18 +1,12 @@
 fn main() {
-    let s = String::from("hello");
+    let s1 = String::from("hello");
 
-    takes_ownership(s);
-
-    let x = 5;
-
-    makes_copy(x);
-
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{s2}' is {len}.");
 }
 
-fn takes_ownership(some_string: String) {
-    println!("{some_string}");
-}
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
 
-fn makes_copy(some_integer: i32) {
-    println!("{some_integer}");
+    (s, length)
 }
