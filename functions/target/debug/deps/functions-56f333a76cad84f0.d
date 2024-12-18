f5921a342109a029123d12c8937d3e6300347f57
Ian Simons <iansimons93@gmail.com>
2024-12-18 10:29:52 -0500

    learning functions

---
+++ functions/target/debug/deps/functions-56f333a76cad84f0.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/functions/target/debug/deps/functions-56f333a76cad84f0: src/main.rs
+
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/functions/target/debug/deps/functions-56f333a76cad84f0.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/functions/target/debug/functions: /mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/functions/src/main.rs
