b7e800ebe3c6510d4b660aa01e89074074a600a5
Ian Simons <iansimons93@gmail.com>
2024-12-18 08:58:15 -0500

    learning about mut/immut variables

---
+++ variables/target/debug/deps/variables-471e861027a37ec1.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/variables/target/debug/deps/variables-471e861027a37ec1: src/main.rs
+
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/variables/target/debug/deps/variables-471e861027a37ec1.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/variables/target/debug/variables: /mnt/c/Users/ianms/Documents/ScriptingPractice/Rust/learningRust/variables/src/main.rs
