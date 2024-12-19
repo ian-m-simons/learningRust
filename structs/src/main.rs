edd8d8b0ab09e2c4f201e3e6438b77fb4788952d
Ian Simons <iansimons93@gmail.com>
2024-12-19 10:30:46 -0500

    learning methods

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -2,15 +2,15 @@ struct Rectangle {
     length: u32,
     width: u32,
 }
-
+impl Rectangle {
+    fn area(&self) -> u32{
+        self.width * self.length
+    }
+}
 fn main() {
     let rect1 = Rectangle {
         length: 50,
         width: 30,
     };
-    println!("area = {}", area(&rect1));
-}
-
-fn area(rectangle: &Rectangle) -> u32 { 
-    rectangle.length * rectangle.width
+    println!("area = {}", rect1.area());
 }
