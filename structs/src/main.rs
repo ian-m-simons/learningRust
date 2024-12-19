b690a28e0bbc87de51f708a66784521cb6e6b439
Ian Simons <iansimons93@gmail.com>
2024-12-19 10:11:22 -0500

    using struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,40 +1,16 @@
-struct User{
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+struct Rectangle {
+    length: u32,
+    width: u32,
 }
-struct Color (i32, i32, i32);
-struct Point (i32, i32, i32);
 
 fn main() {
-    let user1_email = String::from("someone@example.com");
-    let user1_username = String::from("someusername123");
-
-
-    let user1 = build_user(user1_email, user1_username);
-    let user2 = User {
-        active: user1.active,
-        username: user1.username,
-        email: String::from("another@example.com"),
-        sign_in_count: user1.sign_in_count,
+    let rect1 = Rectangle {
+        length: 50,
+        width: 30,
     };
-    let user3 = User{
-        email: String::from("aThird@example.com"),
-        ..user2
-    };
-
-    println!("{}", user2.email);
-    println!("{}", user3.email);
-    let black = Color(0, 0, 0);
-    let origin = Point(0, 0, 0);
+    println!("area = {}", area(&rect1));
 }
 
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
+fn area(rectangle: &Rectangle) -> u32 { 
+    rectangle.length * rectangle.width
 }
