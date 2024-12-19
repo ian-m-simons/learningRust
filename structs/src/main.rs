a5dd672c6e491d1ad9c7bc1319de49babca0ec6f
Ian Simons <iansimons93@gmail.com>
2024-12-19 10:34:08 -0500

    demonstrating I can write my own methods, not just follow the book

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,11 +6,14 @@ impl Rectangle {
     fn area(&self) -> u32{
         self.width * self.length
     }
+    fn perimeter(&self) -> u32 {
+        (self.width * 2) + (self.length * 2)
+    }
 }
 fn main() {
     let rect1 = Rectangle {
         length: 50,
         width: 30,
     };
-    println!("area = {}", rect1.area());
+    println!("area = {}\nperimeter = {}", rect1.area(), rect1.perimeter());
 }
