6a33bfb3ab7f0e96f009d60db8f1b9e966fe7893
Ian Simons <iansimons93@gmail.com>
2024-12-19 12:23:26 -0500

    implementing options in match

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -22,11 +22,20 @@ fn value_in_cents(coin: Coin) -> u8 {
             25}
     }
 }
-
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+    }
+}
 fn main() {
     let coin1 = Coin::Quarter(UsState::Alaska);
 
     println!("{}", value_in_cents(coin1));
-
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{six:?}");
+    println!("{none:?}");
 
 }
